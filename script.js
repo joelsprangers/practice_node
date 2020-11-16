92dbdc77d9668137666e180e6cc7cf0039f3d4b9
joelsprangers <68640121+joelsprangers@users.noreply.github.com>
2020-11-16 10:38:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten");
+
+//Dit is een grote som:
+let multiplyNumbers = 1230941 * 1823794;
+let devideNumbers = 637263 / 54 ;
+/*
+console.log(multiplyNumbers);
+console.log(devideNumbers);
+*/
